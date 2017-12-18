732ad0cb113e3308c34b33ee022b88135923d6a1
Saveliy <samoylov3@hotmail.com>
2017-12-18 01:40:24 -0500

    responsive video

--- main.js
+++ main.js
@@ -87,7 +87,7 @@ window.addEventListener("resize", () => {
 })
 
 
-// DropDown ===============================================
+// DropDown ===============================================================
 ///////////////////////////////////////////////////////////////////////////
 
 const vaDropdowns = document.querySelectorAll('.va__Dropdown');
@@ -109,4 +109,16 @@ Array.from(vaDropdowns).forEach(dd => {
         }
 
     })
+})
+
+// Responsive Video Ratio (16:9) ==========================================
+///////////////////////////////////////////////////////////////////////////
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth < 950) {
+        const vaVideoPageContent = document.querySelector('.va__Body__VideoPageContent');
+        const vaVideoPageContentWidth = vaVideoPageContent.offsetWidth;
+        const vaVideoPlayerBox = document.querySelector('.vpc__VideoBox__Video');
+        vaVideoPlayerBox.style.height = (vaVideoPageContentWidth / 1.77777777) + "px";
+    }
 })
